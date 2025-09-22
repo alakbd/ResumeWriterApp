6e0337d286c76ae725de9bcff01726c088a5b210
alakbd <74824186+alakbd@users.noreply.github.com>
2025-09-22 20:45:20 +0100

    Add files via upload
    adding

---
+++ app/src/main/java/com/example/resumewriter/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.resumewriter
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://resume-writer-04uv.onrender.com/")
+        setContentView(webView)
+    }
+}
