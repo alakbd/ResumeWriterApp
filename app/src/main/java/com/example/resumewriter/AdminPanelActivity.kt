c2726dc62e505c662ed37b4e32729d5c26333bbf
alakbd <74824186+alakbd@users.noreply.github.com>
2025-09-24 17:00:21 +0100

    Update AdminPanelActivity.kt
    u

--- app/src/main/java/com/example/resumewriter/AdminPanelActivity.kt
+++ app/src/main/java/com/example/resumewriter/AdminPanelActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import android.view.View
 import android.content.Intent
 import android.content.Context
-import android.app.AlertDialog
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
