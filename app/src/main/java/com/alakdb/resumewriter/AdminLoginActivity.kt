0c72ffa5adb6ce4f9c0f266f8de0acbf057ebcaf
alakbd <74824186+alakbd@users.noreply.github.com>
2025-09-26 16:29:54 +0100

    Update AdminLoginActivity.kt
    u

--- app/src/main/java/com/alakdb/resumewriter/AdminLoginActivity.kt
+++ app/src/main/java/com/alakdb/resumewriter/AdminLoginActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.alakdb.resumewriter.databinding.ActivityAdminLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class AdminLoginActivity : AppCompatActivity() {
