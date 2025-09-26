bb60451ef543c31ab1d9b9801eb675f7bb2443b1
alakbd <74824186+alakbd@users.noreply.github.com>
2025-09-26 18:43:30 +0100

    Update MainActivity.kt
    u

--- app/src/main/java/com/alakdb/resumewriter/MainActivity.kt
+++ app/src/main/java/com/alakdb/resumewriter/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alakdb.resumewriter.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.alakdb.resumewriter.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
