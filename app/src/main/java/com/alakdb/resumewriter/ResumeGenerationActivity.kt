11423d0e2726f3688b8386e44e7dfa90c212f99e
alakbd <74824186+alakbd@users.noreply.github.com>
2025-10-14 01:44:50 +0100

    Update ResumeGenerationActivity.kt
    e

--- app/src/main/java/com/alakdb/resumewriter/ResumeGenerationActivity.kt
+++ app/src/main/java/com/alakdb/resumewriter/ResumeGenerationActivity.kt
@@ -174,7 +174,7 @@ class ResumeGenerationActivity : AppCompatActivity() {
         }
     }
 
-    fun isConnected(context: Context): Boolean {
+    fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
