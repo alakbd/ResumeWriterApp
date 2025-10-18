d9e0423e8b7713ff8243e12fe4b9ef83ecd5cadd
alakbd <74824186+alakbd@users.noreply.github.com>
2025-10-19 00:10:06 +0100

    Create Utils.kt
    e

---
+++ app/src/main/java/com/alakdb/resumewriter/Utils.kt
@@ -0,0 +1,6 @@
+import java.security.MessageDigest
+
+fun String.sha256(): String {
+    val bytes = MessageDigest.getInstance("SHA-256").digest(this.toByteArray())
+    return bytes.joinToString("") { "%02x".format(it) }
+}
