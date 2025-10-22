7af6934e3a9bb209e94572fa87e7eed73c0d6256
alakbd <74824186+alakbd@users.noreply.github.com>
2025-10-22 15:21:55 +0100

    Update LoginActivity.kt
    w

--- app/src/main/java/com/alakdb/resumewriter/LoginActivity.kt
+++ app/src/main/java/com/alakdb/resumewriter/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
