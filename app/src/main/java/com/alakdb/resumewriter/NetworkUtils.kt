7734fcf85c231f380dabf27f222dccef8e76122e
alakbd <74824186+alakbd@users.noreply.github.com>
2025-11-06 00:47:43 +0000

    Create NetworkUtils.kt
    w

---
+++ app/src/main/java/com/alakdb/resumewriter/NetworkUtils.kt
@@ -0,0 +1,51 @@
+package com.alakdb.resumewriter
+
+import android.content.Context
+import android.provider.Settings
+import android.util.Log
+import java.net.NetworkInterface
+import java.util.Collections
+
+object NetworkUtils {
+    
+    /**
+     * Get device ID (Android ID)
+     */
+    fun getDeviceId(context: Context): String {
+        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID) ?: "unknown_device"
+    }
+    
+    /**
+     * Get device model and manufacturer
+     */
+    fun getDeviceInfo(): String {
+        return "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}"
+    }
+    
+    /**
+     * Get user agent string
+     */
+    fun getUserAgent(): String {
+        return System.getProperty("http.agent") ?: "Unknown"
+    }
+    
+    /**
+     * Get local IP address
+     */
+    fun getLocalIpAddress(): String {
+        return try {
+            val interfaces = Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (intf in interfaces) {
+                val addrs = Collections.list(intf.inetAddresses)
+                for (addr in addrs) {
+                    if (!addr.isLoopbackAddress && addr.isSiteLocalAddress) {
+                        return addr.hostAddress ?: "unknown_ip"
+                    }
+                }
+            }
+            "unknown_ip"
+        } catch (e: Exception) {
+            "unknown_ip"
+        }
+    }
+}
