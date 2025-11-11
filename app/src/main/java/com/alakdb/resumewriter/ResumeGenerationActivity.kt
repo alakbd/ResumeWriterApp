792630138a50682a29da6deb9966dec4cdaab84a
alakbd <74824186+alakbd@users.noreply.github.com>
2025-11-11 02:59:09 +0000

    Update ResumeGenerationActivity.kt
    w

--- app/src/main/java/com/alakdb/resumewriter/ResumeGenerationActivity.kt
+++ app/src/main/java/com/alakdb/resumewriter/ResumeGenerationActivity.kt
@@ -359,10 +359,10 @@ class ResumeGenerationActivity : AppCompatActivity() {
         //binding.btnDebugAuth.setOnClickListener {debugAuthFlow()}
     }
 
-    private val textWatcher = object : android.text.TextWatcher {
+    private val textWatcher = object : TextWatcher {
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-    override fun afterTextChanged(s: android.text.Editable?) {
+    override fun afterTextChanged(s: Editable?) {
         checkGenerateButtonState()
     }
 }
